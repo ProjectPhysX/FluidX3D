2fcebc08840c9c825271dfad773f34ce5ff55b60
Brian Cardarella <bcardarella@gmail.com>
2023-01-28 12:35:52 -0500

    Change bin name for macos
    .exe is an extension format common on Windows but not on MacOS. Generating the bin without the extension is just fine

--- make.sh
+++ make.sh
@@ -6,7 +6,7 @@ rm -f ./bin/FluidX3D.exe # prevent execution of old version if compiling fails
 #g++ ./src/*.cpp -o ./bin/FluidX3D.exe -std=c++17 -pthread -I./src/OpenCL/include -L./src/OpenCL/lib -lOpenCL -I./src/X11/include -L./src/X11/lib -lX11 # compile on Linux with X11
 
 g++ ./src/*.cpp -o ./bin/FluidX3D.exe -std=c++17 -pthread -I./src/OpenCL/include -L./src/OpenCL/lib -lOpenCL # compile on Linux (without X11)
-#g++ ./src/*.cpp -o ./bin/FluidX3D.exe -std=c++17 -pthread -I./src/OpenCL/include -framework OpenCL # compile on macOS (without X11)
+#g++ ./src/*.cpp -o ./bin/FluidX3D -std=c++17 -pthread -I./src/OpenCL/include -framework OpenCL # compile on macOS (without X11)
 #g++ ./src/*.cpp -o ./bin/FluidX3D.exe -std=c++17 -pthread -I./src/OpenCL/include -L/system/vendor/lib64 -lOpenCL # compile on Android (without X11)
 
 ./bin/FluidX3D.exe $1 # run FluidX3D
