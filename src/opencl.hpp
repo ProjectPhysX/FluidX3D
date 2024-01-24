82976f15d2bd20b9188ea623cf0bac046c6c81ce
Dr. Moritz Lehmann <dr.moritz.lehmann@gmail.com>
2024-01-24 20:54:23 +0100

    Workaround for Nvidia driver bug: enqueueFillBuffer is broken for large buffers on Nvidia GPUs

--- src/opencl.hpp
+++ src/opencl.hpp
@@ -339,10 +339,11 @@ public:
 		if(host_buffer_exists) {
 			std::fill(host_buffer, host_buffer+range(), value); // faster than "for(ulong i=0ull; i<range(); i++) host_buffer[i] = value;"
 		}
-		if(device_buffer_exists) {
-			cl_queue.enqueueFillBuffer(device_buffer, value, 0ull, capacity()); // faster than "write_to_device();"
-			cl_queue.finish();
-		}
+		write_to_device(); // enqueueFillBuffer is broken for large buffers on Nvidia GPUs!
+		//if(device_buffer_exists) {
+		//	cl_queue.enqueueFillBuffer(device_buffer, value, 0ull, capacity()); // faster than "write_to_device();"
+		//	cl_queue.finish();
+		//}
 	}
 	inline const ulong length() const { return N; }
 	inline const uint dimensions() const { return d; }
